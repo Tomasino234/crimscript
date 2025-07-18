35d695991f598d19e58c3517f44b0eabe8f7f72b
Tomasino234 <tomas.strezo@gmail.com>
2025-07-19 01:12:57 +0200

    remote rename bypass added

--- loader.lua
+++ loader.lua
@@ -1,3 +1,28 @@
+local oldNewIndex
+local usedtable = {}
+oldNewIndex = hookmetamethod(game, "__newindex", function(self, key, value)
+    -- Check if setting the Name of a RemoteEvent-like object
+    if typeof(self) == "Instance"
+        and (self:IsA("RemoteEvent") or self:IsA("RemoteFunction") or self:IsA("BindableEvent") or self:IsA("BindableFunction"))
+        and key == "Name"
+        and (value == "" or value == " ")
+        and not usedtable[self.Name] then
+
+        local ogname = self.Name
+        
+        usedtable[self.Name] = true
+
+        task.delay(0.1, function()
+            self.Name = ogname
+        end)
+
+        -- Optional: Save it, tag it, or compare to a target
+        -- Example: if self.Name == "TargetRemote" then do something
+    end
+
+    return oldNewIndex(self, key, value)
+end)
+
 getgenv().Config = {
 	Invite = "informant.wtf",
 	Version = "0.0",
