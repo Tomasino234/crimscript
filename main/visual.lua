9891a1e44404f8f0ef447254f5afd303b4119690
Tomasino234 <tomas.strezo@gmail.com>
2025-07-19 13:15:08 +0200

    df

--- main/visual.lua
+++ main/visual.lua
@@ -162,6 +162,7 @@ function getChamColor(humanoid)
         return cham_color
     elseif cham_color_mode == "Health" then
         local num = math.clamp(humanoid.Health/humanoid.MaxHealth, 0, 1)
+        local color = Color3.new(0,1,0):Lerp(Color3.new(1,0,0), num)
         return num
     end
 end
